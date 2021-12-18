a0607adba6c7cc29dec3eb20e25915b9fed79d57
Dzegbe <birade-madra@hotmail.com>
2021-12-18 15:01:24 +0100

    premier commit

---
+++ src/main/java/be/technofutur/haveyourstyle/HaveYourStyleApplication.java
@@ -0,0 +1,13 @@
+package be.technofutur.haveyourstyle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaveYourStyleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HaveYourStyleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/be/technofutur/haveyourstyle/HaveYourStyleApplicationTests.java
@@ -0,0 +1,13 @@
+package be.technofutur.haveyourstyle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaveYourStyleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
